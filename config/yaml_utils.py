9965bd12fe7a567609567ad0f761d674411c2af6
viGor027 <52599269+viGor027@users.noreply.github.com>
2024-12-28 20:32:36 +0100

    Corrected yaml_utils docstrings

--- config/yaml_utils.py
+++ config/yaml_utils.py
@@ -6,10 +6,6 @@ def save_dict_to_yaml(dct: dict, path: str, cfg_file_name: str):
     """
     Saves a dictionary to a YAML file.
 
-    This function takes a dictionary and saves it as a YAML file at the specified
-    path with the given file name. The file name should not include the file extension,
-    as ".yaml" is automatically appended.
-
     Args:
         dct (dict): The dictionary to be saved to a YAML file.
         path (str): The directory where the YAML file will be saved.
@@ -24,12 +20,9 @@ def yaml_to_dict(path: str, file_name: str) -> dict:
     """
     Loads a YAML file and converts its contents to a dictionary.
 
-    This function reads a YAML file from the specified path and returns its contents
-    as a Python dictionary. The file name should include the ".yaml" extension.
-
     Args:
         path (str): The directory path where the YAML file is located.
-        file_name (str): The name of the YAML file (including the file extension).
+        file_name (str): The name of the YAML file **(including the file extension)**.
 
     Returns:
         dict: The contents of the YAML file as a Python dictionary.
