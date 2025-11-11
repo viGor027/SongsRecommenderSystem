250c85e81d6cd376479e3e7390079a2478d68649
viGor027 <vigor.wm@yahoo.com>
2025-11-11 13:35:21 +0100

    Implemented offline normalization, refactored label encoding to class

--- workflow_actions/dataset_preprocessor/source/label_encoder.py
+++ workflow_actions/dataset_preprocessor/source/label_encoder.py
@@ -5,40 +5,49 @@ from torch.nn.functional import one_hot
 from collections import Counter
 
 
-def create_label_mapping() -> dict[int, str]:
-    """Creates mapping tag -> number for every tag present in labels.json"""
-    if not LABELS_PATH.exists():
-        raise FileExistsError("Labels file labels.json doesn't exist.")
-
-    song_to_labels = read_json_to_dict(LABELS_PATH)
-    all_tags = []
-    for tags_list in song_to_labels.values():
-        all_tags.extend(tags_list)
-    tags = Counter(all_tags).keys()
-
-    mapping = {label: idx for idx, label in enumerate(sorted(list(tags)))}
-    write_dict_to_json(data=mapping, file_path=LABEL_MAPPING_PATH)
-    return mapping
-
-
-def encode_song_labels_to_multi_hot_vector(song_title: str) -> torch.Tensor:
-    """
-    Encodes songs tags to multi-hot vector.
-    **Pass song title without extension.**
-    """
-    if not LABEL_MAPPING_PATH.exists():
-        raise FileExistsError(
-            (
-                "Labels file label_mapping.json doesn't exist. "
-                "Call label_encoder.create_label_mapping to create it."
+class LabelEncoder:
+    def __init__(self):
+        if not LABEL_MAPPING_PATH.exists():
+            raise FileExistsError(
+                (
+                    "Labels file label_mapping.json doesn't exist. "
+                    "Call label_encoder.create_label_mapping to create it."
+                )
             )
-        )
-    if not LABELS_PATH.exists():
-        raise FileExistsError("Labels file labels.json doesn't exist.")
-    labels = read_json_to_dict(LABELS_PATH)
-    label_to_int = read_json_to_dict(LABEL_MAPPING_PATH)
-    n_classes = len(label_to_int)
-    song_int_tags = [label_to_int[label] for label in labels[song_title]]
-    one_hot_tags = one_hot(torch.tensor(song_int_tags), num_classes=n_classes)
-    multi_hot_tags = one_hot_tags.sum(dim=0).clamp(max=1).float()
-    return multi_hot_tags
+        if not LABELS_PATH.exists():
+            raise FileExistsError("Labels file labels.json doesn't exist.")
+
+        self.song_to_labels = read_json_to_dict(LABELS_PATH)
+        self.label_to_int = read_json_to_dict(LABEL_MAPPING_PATH)
+
+        self.n_classes = len(self.label_to_int)
+
+        self.EXCLUDED_TAGS = ["N/A"]
+
+    def create_label_mapping(self) -> dict[int, str]:
+        """Creates mapping tag -> number for every tag present in labels.json"""
+        if not LABELS_PATH.exists():
+            raise FileExistsError("Labels file labels.json doesn't exist.")
+
+        all_tags = []
+        for tags_list in self.song_to_labels.values():
+            tags_list = [tag for tag in tags_list if tag not in self.EXCLUDED_TAGS]
+            all_tags.extend(tags_list)
+        tags = Counter(all_tags).keys()
+        mapping = {label: idx for idx, label in enumerate(sorted(list(tags)))}
+        write_dict_to_json(data=mapping, file_path=LABEL_MAPPING_PATH)
+        return mapping
+
+    def encode_song_labels_to_multi_hot_vector(self, song_title: str) -> torch.Tensor:
+        """
+        Encodes songs tags to multi-hot vector.
+        **Pass song title without extension.**
+        """
+        song_int_tags = [
+            self.label_to_int[label]
+            for label in self.song_to_labels[song_title]
+            if label not in self.EXCLUDED_TAGS
+        ]
+        one_hot_tags = one_hot(torch.tensor(song_int_tags), num_classes=self.n_classes)
+        multi_hot_tags = one_hot_tags.sum(dim=0).clamp(max=1).float()
+        return multi_hot_tags
---
+++ workflow_actions/dataset_preprocessor/source/offline_normalizer.py
@@ -0,0 +1,68 @@
+import torch
+from typing import Literal
+from torch.utils.data import DataLoader
+from workflow_actions.paths import MODEL_READY_TRAIN_DIR
+from functools import partial
+
+
+class OfflineNormalizer:
+    def __init__(
+        self,
+        n_mels: int,
+        normalization_type=Literal["per_mel"],
+    ):
+        from workflow_actions.train.source.dataloading.fragments_dataset import (
+            FragmentsDataset,
+        )
+
+        self._train_loader = DataLoader(
+            FragmentsDataset("train"),
+            batch_size=64,
+            shuffle=True,
+            collate_fn=FragmentsDataset.collate_concat,
+            num_workers=0,
+            pin_memory=False,
+        )
+        self._n_mels = n_mels
+        compute_norm_map = {
+            "per_mel": (
+                partial(self.compute_norm_values_per_mel, n_mels=self._n_mels),
+                self._apply_compute_norm_values_per_mel,
+            )
+        }
+        self._norm_package = compute_norm_map[normalization_type]
+
+    def __call__(self):
+        param_function, apply_function = self._norm_package
+        params = param_function()
+        apply_function(*params)
+
+    def compute_norm_values_per_mel(self, n_mels: int, eps: float = 1e-6):
+        """
+        Computes mu_f i std_f per mel based on TRAINING SET.
+        returns: (mu_f, std_f) both having shape (F,).
+        """
+        sum_f = torch.zeros(n_mels, dtype=torch.float64)  # (F,)
+        sumsq_f = torch.zeros(n_mels, dtype=torch.float64)  # (F,)
+        count = 0
+
+        for X, _ in self._train_loader:  # X: (B, F, T)
+            x = X.to(torch.float64)
+            sum_f += x.sum(dim=(0, 2))  # (F,)
+            sumsq_f += (x * x).sum(dim=(0, 2))  # (F,)
+            count += x.shape[0] * x.shape[2]  # B * T
+
+        mu_f = (sum_f / count).float()  # (F,)
+        var_f = (sumsq_f / count - (sum_f / count) ** 2).clamp_min(0.0)  # (F,)
+        std_f = var_f.sqrt().float() + eps  # (F,)
+        return mu_f, std_f
+
+    def _apply_compute_norm_values_per_mel(self, mu_f, std_f):
+        # from (F,) to (1, F, 1)
+        mu = mu_f.view(1, -1, 1)
+        std = std_f.view(1, -1, 1)
+
+        for p in sorted(MODEL_READY_TRAIN_DIR.glob("X_*.pt")):
+            x = torch.load(p)  # (1, F, T) float32
+            x = (x - mu) / std  # broadcast po F i T
+            torch.save(x, p)
