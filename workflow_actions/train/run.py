123d3e3851187ed0a5920b21c80051d3c2df093e
viGor027 <52599269+viGor027@users.noreply.github.com>
2025-12-23 10:02:21 +0100

    resnet train config

--- workflow_actions/train/run.py
+++ workflow_actions/train/run.py
@@ -4,8 +4,16 @@ if __name__ == "__main__":
     from workflow_actions.json_handlers import read_json_to_dict
     from multiprocessing import freeze_support
 
+    BASE_RUN_NAME = "_IMAGENET1K_V1_no_aug"
+
     freeze_support()
-    configs = read_json_to_dict(TRAIN_CONFIG_PATH)
-    for training_cfg in configs:
+    training_cfg = read_json_to_dict(TRAIN_CONFIG_PATH)
+    for backbone in ["resnet18", "resnet34", "resnet50", "resnet101"]:
+        training_cfg["run_single_training_config"]["architecture"]["backbone_name"] = (
+            backbone
+        )
+        training_cfg["run_single_training_config"]["run_name"] = (
+            backbone + BASE_RUN_NAME
+        )
         train = Train(**training_cfg)
         train.run_single_training()
